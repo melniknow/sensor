3ac46509172d8ec78c1ec9cd2df395042bc73924
Sergey Melnikov <smelnikovimit@gmail.com>
2023-06-24 09:40:52 +0600

    Init

---
+++ create_database_and_tables.sql
@@ -0,0 +1,22 @@
+-- Create database & user
+-- > sudo su postgres
+-- > psql
+
+create database sensor;
+create user sensor_owner with password '1234';
+grant all privileges on database sensor to sensor_owner;
+
+-- Create tables:
+
+create table sensor(
+    id int primary key generated always as identity,
+    name varchar(100) not null unique
+);
+
+create table measurement(
+    id int generated always as identity,
+    value double precision not null,
+    raining boolean not null,
+    measurement_date_time timestamp not null,
+    sensor varchar(100) references sensor(name)
+);
